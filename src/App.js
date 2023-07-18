d55f3018b6e1324393b21b7050c02b7e9192d53c
Edin <edin.vatrich@gmail.com>
2023-07-18 17:43:40 +0200

    First upload

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import ContactCard from "./ContactCard";
+
+function App() {
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    fetch("https://randomuser.me/api/?results=5")
+      .then((response) => response.json())
+      .then((data) => {
+        setResults(data.results);
+      });
+  }, []);
+
+  return (
+    <div>
+      {results.map((result, index) => {
+        return (
+          <ContactCard
+            key={index}
+            avatarUrl={result.picture.large}
+            name={result.name.first}
+            email={result.email}
+            age={result.dob.age}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+export default App;
